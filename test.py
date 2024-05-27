7eba8cca3c637e2571380fa281b505eb87414050
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-05-27 18:07:09 +0530

    this is testing file where i am testing the python code working properly or not

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 #this file we are creating for testing the code is it working properly or not!!
 # so calling the module
-from src.loggers import logging
+from src.logger import logging
 from src.exception import CustomException
 import os,sys
+from src.components.data_ingestion import DataIngestion,DataIngestionConfig
 
 
 def main():
     try:
-       pass
+       #creating an object of DataIngestion class
+       di = DataIngestion()
+       di.initialize_data_ingestion()
 
     except Exception as e:
         raise CustomException(e,sys)
