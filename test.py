66c44367893de23f1d3bbb483f030b745cf6c37f
Raees Azam Shaikh <shaikhraishazam@gmail.com>
2024-07-05 10:39:07 +0530

    this is testing file is like a training pipeline where we train our model

--- test.py
+++ test.py
@@ -4,13 +4,23 @@ from src.logger import logging
 from src.exception import CustomException
 import os,sys
 from src.components.data_ingestion import DataIngestion,DataIngestionConfig
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 
 def main():
     try:
        #creating an object of DataIngestion class
        di = DataIngestion()
-       di.initialize_data_ingestion()
+       raw_path,train_path,test_path = di.initialize_data_ingestion()
+
+       #creating Datatransformation class object
+       dt = DataTransformation()
+       train_array,test_array = dt.InitiateDataTransformation(raw_path,train_path,test_path)
+
+       #creating an object of ModelTrainer class
+       mt = ModelTrainer()
+       mt.initialize_model_training(train_array,test_array)
 
     except Exception as e:
         raise CustomException(e,sys)
